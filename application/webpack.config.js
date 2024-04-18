c5c09251c91602593fce12175cd51d256dfee389
Achim Krämer <72191461+achim-kraemer@users.noreply.github.com>
2024-04-18 08:03:09 +0200

    main: pwa created

--- application/webpack.config.js
+++ application/webpack.config.js
@@ -1,5 +1,37 @@
 const Encore = require('@symfony/webpack-encore');
 
+const OfflinePlugin = require("@lcdp/offline-plugin");
+// var imageCacheBuster = (Math.random() + 1).toString(36).substring(7);
+const manifestOptions = {
+    name: "prompt privacy portal", // Erscheint unter anderem im App-Startbildschirm auf Android.
+    short_name: "prompt privacy portal", // App-Titel z.b. im Android App-Drawer.
+    description: "prompt privacy portal PWA",
+    // Einfärbungen der Browserleiste.
+    theme_color: "#FFFFFF",
+    background_color: "#FFFFFF",
+    display: "minimal-ui", // Browser stellt im App-Modus nur einen Reload und Vor/Zurückbutton bereit.
+    prefer_related_applications: false, // Legt fest das keine alternativ Apps zur Installation angeboten werden sollen.
+    start_url: "/chat/", // Diese URL wird beim öffnen der App geladen. Könnte z.b. auch /?pwa=true sein falls man es tracken möchte.
+    icons: [
+        {
+            src: "/build/images/favicon/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+        },
+        {
+            src: "/build/images/favicon/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+        },
+        {
+            src: "/build/images/favicon/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable", // Leg fest das das Icon maskierbar ist, also von Android in den diversen Stilen zugeschnitten werden darf. @see https://maskable.app/editor
+        },
+    ],
+};
+
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
@@ -75,6 +107,36 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+
+    //For manifest.json
+    .configureFilenames({
+        js: '[name].[contenthash].js',
+        css: '[name].[contenthash].css',
+        assets: 'assets/[name].[hash:8].[ext]'
+    })
+        // Manifest hinzufügen.
+        .configureManifestPlugin((options) => {
+            options.seed = manifestOptions;
+        });
+
+    const webpackConfig = Encore.getWebpackConfig();
+
+    webpackConfig.plugins.push(new OfflinePlugin({
+        strategy: 'changed',
+        responseStrategy: 'cache-first', // Bedeutet das der Nutzer die Daten erst aus den Cache laden soll und als Fallback aus dem Web.
+        caches: {
+            main: [
+                '*.css',
+                '*.js',
+                'fonts/*',
+                'images/*'
+            ]
+        },
+        ServiceWorker: {
+            output: '../sw.js' // Ausgabe-Pfad des Service-Worker, der Service Worker muss direkt im "/public"-Verzeichnis liegen, er darf sich nicht in einem Unterordner befinden.
+        }
+    }))
 ;
 
 module.exports = Encore.getWebpackConfig();
