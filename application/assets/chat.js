3b1c74def0cf64cd1846ac7e1394728f7736c6e8
Achim Krämer <72191461+achim-kraemer@users.noreply.github.com>
2024-11-20 19:01:09 +0100

    main: o1-preview and o1-mini added

--- application/assets/chat.js
+++ application/assets/chat.js
@@ -24,7 +24,7 @@ const settingsCloseButton = document.getElementById("settings-close-btn");
 const deleteUserButtons = document.querySelectorAll('button.delete-user-btn');
 
 settingsModal.style.display = "none";
-const defaultChatType = 'gpt-4o';
+const defaultChatType = 'o1-preview';
 let userText = null;
 const initialHeight = chatInput.scrollHeight;
 let sessionId = null;
